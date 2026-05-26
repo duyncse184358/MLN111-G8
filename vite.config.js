f62c22f363831e36846dfc9744e553dd1f83d2a7
duyncse184358 <duyncse184358@fpt.edu.vn>
2026-05-26 15:24:43 +0700

    update - VS1

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: './CaseStudy_Chapter3',
+  server: {
+    port: 3000,
+    open: true
+  }
+})
+
